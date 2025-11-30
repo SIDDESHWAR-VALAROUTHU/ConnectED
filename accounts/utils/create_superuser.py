89fed22a264f9820f3b1e0b8e859cc51271ebc78
Siddeshwar <connectedalumninetwork@gmail.com>
2025-11-30 20:19:36 +0530

    Fix superuser creation fields

--- accounts/utils/create_superuser.py
+++ accounts/utils/create_superuser.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import get_user_model
+from accounts.models import CustomUser as User
 
 def create_superuser():
-    User = get_user_model()
     if not User.objects.filter(username="admin").exists():
         User.objects.create_superuser(
             uid="admin1",
+            username="admin1",
             email="sidhusiddeshwar01@gmail.com",
             password="Admin@123"
         )
